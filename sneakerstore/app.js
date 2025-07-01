3741e7ef545380d6ff9adf169cd5e845cb054708
rick695 <rickyleongkm@gmail.com>
2025-07-01 15:20:53 +0800

    Add files via upload

---
+++ sneakerstore/app.js
@@ -0,0 +1,152 @@
+
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 1,
+    colors: [
+      {
+        code: "black",
+        img: "./img/Product/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/Product/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 2,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/Product/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/Product/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 1.50,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/Product/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/Product/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 3,
+    colors: [
+      {
+        code: "black",
+        img: "./img/Product/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/Product/crater2.png",
+      },
+    ],
+  },
+  {
+    
+    id: 5,
+    title: "Hippie",
+    price: 4,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/Product/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/Product/hippie2.png",
+      },
+    ],
+  },
+  {id: 6,
+    title: "MSCHF",
+    price: 99,
+    colors: [
+      {
+        code: "red",
+        img: "./img/Product/Big Red.png",
+      },
+      {
+        code: "blue",
+        img: "./img/Product/no stock.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    wrapper.style.width = `${ 100 * products.length}vw`;
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    choosenProduct = products[index];
+
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "RM" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
+  
